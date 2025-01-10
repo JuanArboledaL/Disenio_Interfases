afa5e231d9f2a9e2519f85e1f5c37d443bee3a44
JuanArboledaL <juanarboledalopez@gmail.com>
2025-01-10 12:00:56 +0100

    WPF 10/01/2025

---
+++ TemaWPF/RevolucionesCocheWPF/RevolucionesCocheWPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RevolucionesCocheWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void acelerador_Click(object sender, RoutedEventArgs e)
+        {
+            RotateTransform transform = new RotateTransform();
+            transform.Angle = 28;
+        }
+
+        private void freno_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
