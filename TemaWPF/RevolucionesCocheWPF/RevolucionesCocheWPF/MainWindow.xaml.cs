cc72bcab9246d58119de35d40d96695b12215415
JuanArboledaL <juanarboledalopez@gmail.com>
2025-01-15 14:44:46 +0100

    coche
    15/01/2025

--- TemaWPF/RevolucionesCocheWPF/RevolucionesCocheWPF/MainWindow.xaml.cs
+++ TemaWPF/RevolucionesCocheWPF/RevolucionesCocheWPF/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,21 +22,142 @@ namespace RevolucionesCocheWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Boolean interruptor = false;
+        private int revoluciones;
         public MainWindow()
         {
             InitializeComponent();
+            RotateTransform transform = new RotateTransform();
+            
         }
-
 
         private void acelerador_Click(object sender, RoutedEventArgs e)
         {
-            RotateTransform transform = new RotateTransform();
-            transform.Angle = 28;
+            if (interruptor == true) {
+
+                if (sport.IsChecked == true)
+                {
+                   
+                    modSport();
+                }
+                else if (modEco.IsChecked == true)
+                {
+
+                    eco();
+                }
+                else { 
+                
+                    normal();
+                }
+            }
         }
 
         private void freno_Click(object sender, RoutedEventArgs e)
         {
-           
+            if (interruptor == true) {
+
+                if (rotarAguja.Angle > 0) {
+
+                    if (rotarAguja.Angle < 0)
+                    {
+                        rotarAguja.Angle = 0;
+                        velocimetro.Content ="0/Km";
+                    }
+                    else {
+
+                        rotarAguja.Angle = rotarAguja.Angle - 10;
+                        velocimetro.Content = rotarAguja.Angle.ToString() + "/Km";
+
+                    }
+                    
+                }
+            }
+        }
+
+        private void interruptor_Click(object sender, RoutedEventArgs e)
+        {
+
+            barraRevoluciones.Height = 142;
+            rotarAguja.Angle = 24;
+            interruptor = true;
+            encendido.Visibility = Visibility.Visible;
+            apagado.Visibility = Visibility.Hidden;
+            sport.Visibility = Visibility.Visible;
+            modEco.Visibility = Visibility.Visible;
+            modNormal.Visibility = Visibility.Visible;
+            modEco.IsChecked = false;
+            modNormal.IsChecked = false;
+            sport.IsChecked = false;
+
+        }
+
+        public void modSport()
+        {
+
+            if (sport.IsChecked == true) {
+
+                modEco.Visibility = Visibility.Hidden;
+                modNormal.Visibility = Visibility.Hidden;
+
+                if (rotarAguja.Angle < 225 && rotarAguja.Angle > 23)
+                {
+
+                    barraRevoluciones.Height -= 10;
+                    rotarAguja.Angle = rotarAguja.Angle + 15;
+                    velocimetro.Content = rotarAguja.Angle.ToString() + "/Km";
+
+                }
+
+            }
+        }
+
+        public void eco() {
+
+            if (modEco.IsChecked == true) {
+
+                sport.Visibility = Visibility.Hidden;
+                modNormal.Visibility = Visibility.Hidden;
+
+                if (rotarAguja.Angle < 225 && rotarAguja.Angle > 23) {
+
+                    barraRevoluciones.Height -= 6;
+                    rotarAguja.Angle = rotarAguja.Angle + 3;
+                    velocimetro.Content = rotarAguja.Angle.ToString() + "/Km";
+
+                }
+            }
+        }
+
+        public void normal() {
+
+
+            if (modNormal.IsChecked == true) {
+
+                sport.Visibility = Visibility.Hidden;
+                modEco.Visibility = Visibility.Hidden;
+                
+
+                if (rotarAguja.Angle < 225 && rotarAguja.Angle > 23)
+                {
+
+                    barraRevoluciones.Height -= 6;
+                    rotarAguja.Angle = rotarAguja.Angle + 7;
+                    velocimetro.Content = rotarAguja.Angle.ToString() + "/Km";
+                }
+              
+                
+            }  
+        }
+
+        private void encendido_Click(object sender, RoutedEventArgs e)
+        {
+            encendido.Visibility = Visibility.Hidden;
+            apagado.Visibility = Visibility.Visible;
+            modEco.Visibility= Visibility.Hidden;
+            modNormal.Visibility= Visibility.Hidden;
+            sport.Visibility= Visibility.Hidden;
+            velocimetro.Content = "";
+            rotarAguja.Angle = 0;
         }
     }
 }
